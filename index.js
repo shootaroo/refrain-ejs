ab43899936b0d59ff3ffb501447527078fbdd514
shootaroo <shotaro.tsubouchi@gmail.com>
2016-05-13 12:18:04 +0900

    Support options and require

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var ejs = require('ejs');
 
 module.exports = function (text, content, callback) {
   callback(null, ejs.render(text, _.assign(content, {
-    filename: content.filePath,
+    require: require,
     _: require('lodash') //TODO
-  })));
+  }), _.assign({
+    filename: content.filePath
+  }, this.options.ejs)));
 };
